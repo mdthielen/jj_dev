57a1acc2e3d82d5b9f89eccda1cf7499dd3a16cc
mdthielen <mdthielen@gmail.com>
2017-03-06 16:26:35 -0800

    ignore .idea folder update sbtvInfo based on new folder structure added debug into sbtvInfo added sbtvKeyMaker generated new sbtvKey.txt

--- maya_scripts/python/sbtvInfo.py
+++ maya_scripts/python/sbtvInfo.py
@@ -16,7 +16,7 @@
     
     Example usage:
         import sbtvInfo
-        seqInfo = sbtvInfo.SeqInfo('101', '100')
+        seqInfo = sbtvInfo.Info('101', '100')
         seqInfo.allShots
         #Returns:
         #['sh_010', 'sh_020', 'sh_030', 'sh_040', 'sh_050', 'sh_060', 'sh_070', 'sh_080', 'sh_090', 'sh_100', 'sh_110', 'sh_120', 'sh_130', 'sh_140', 'sh_150']
@@ -25,21 +25,18 @@
 
 
 import os
-import sys
 
-if sys.platform == 'darwin':
-    keyFile = r'/Volumes/public/StoryBots/sbtv/03_shared_assets/01_cg/maya_tools/maya_scripts/python/sbtvKey.txt'
-elif sys.platform == 'win32':
-    keyFile = r'C:\Users\rshowalter\Documents\maya\scripts\storybots\python\sbtvKey.txt'
-elif sys.platform == 'linux2':
-    keyFile = r'/home/rshowalter/maya/scripts/storybots/python/sbtvKey.txt'
-else:
-    print('couldn\'t recognize operating system')
-    import sys
-    sys.exit()
-stage = '2_production'
-season = 'season_01'
 
+def main():
+    dev = 0
+    if dev:
+        seq_info = Info('101', '100')
+        anim_publish_files = seq_info.getLastAnim()
+        for publish in anim_publish_files:
+            print publish
+
+        for shot in seq_info.allShots:
+            print shot
 
 def readContents( keyFile ):
 
@@ -51,54 +48,70 @@ def readContents( keyFile ):
 
 
 def getEp( ep, fileContents):
-
     for line in fileContents:
-
         if 'EPISODE' in line:
-
             if ep == line.split(' ')[1]:
-
                 epName = line.split(' ')[2]
                 epID = '_'.join( [ep, epName] )
-
                 return epID, epName
-
     raise NameError('Episode number not found, check key:\n%s'%keyFile)
 
-
 def getSeq( seq, fileContents):
-
     for line in fileContents:
-
         if 'SEQ' in line:
-
             if seq == line.split(' ')[1]:
-
                 seqName = line.split(' ')[2]
                 seqID = '_'.join( [seq, seqName] )
-
                 return seqID, seqName
-
     raise NameError('Seqence number not found, check input or update keyfile:\n    %s'%keyFile)
 
 class Info():
-
     def __init__(self, ep, seq, shot=''):
+        import sys
         self.ep = ep
         self.seq = seq
         self.shot = shot
 
+        keyFile = None
 
-        #import socket
-        #systemCheck = socket.gethostname()
-        
         if sys.platform == 'darwin':
-            project = r'/Volumes/public/StoryBots/sbtv'
+            keyFile = r'/Volumes/public/StoryBots/production/series/ask_the_storybots/03_shared_assets/01_cg/05_maya_tools/' \
+                      r'jj_dev/maya_scripts/python/sbtvKey.txt'
         elif sys.platform == 'win32':
-            project = r'C:\Users\rshowalter\Documents\maya\projects\SBTV'
+            keyFile = r'~\Documents\maya\scripts\storybots\python\sbtvKey.txt'
         elif sys.platform == 'linux2':
-            project = r'/home/rshowalter/maya/projects/SBTV'
-        #expand this later for mac machines, though many other changes may be needed
+            keyFile = r'~/maya/scripts/storybots/python/sbtvKey.txt'
+        else:
+            print('couldn\'t recognize operating system')
+            import sys
+            sys.exit()
+        if keyFile and os.path.exists(keyFile):
+            print('Using keyfile: ' + keyFile)
+        else:
+            print('Warning: missing keyfile --> sbtvKey.txt')
+
+        # stage in production
+        # possible selections: 1_pre-preduction, 2_production, 3_distribution
+        # default = 2_production
+        stage = '2_production'
+
+        project_name = 'ask_the_storybots'
+        local_darwin_linux_folder = 'maya/projects/' + project_name
+
+        if sys.platform == 'darwin':
+            public_mount = '/Volumes/public/StoryBots/production/series/' + project_name
+            local_mount = os.path.expanduser('~/Documents/' + local_darwin_linux_folder)
+            if os.path.exists(public_mount):
+                project_folder = public_mount
+            elif os.path.exists(local_mount):
+                project_folder = local_mount
+            else:
+                print 'No project exists'
+        elif sys.platform == 'win32':
+            project_folder = os.path.join(os.path.expanduser('~\\Documents'), 'maya', 'projects', project_name)
+        elif sys.platform == 'linux2':
+            project_folder = os.path.join(os.path.expanduser('~'), local_darwin_linux_folder, project_name)
+        # expand this later for mac machines, though many other changes may be needed
         else:
             raise EnvironmentError('This system was not recognised')
 
@@ -107,20 +120,16 @@ class Info():
         self.epID, self.epName = getEp( self.ep, fileContents )
         self.seqID, self.seqName = getSeq( self.seq, fileContents )
         self.sequence = 'sq' + self.seqID		
-        self.seqPath = os.path.join(project,season,self.epID,stage,self.sequence)
-        self.allShots = [ x for x in os.listdir(self.seqPath) if 'sh_' in x]
+        self.seqPath = os.path.join(project_folder,self.epID,stage,self.sequence)
+        self.allShots = sorted([ x for x in os.listdir(self.seqPath) if 'sh_' in x])
         # added to fix shot folder naming inconsistencies
         if len(self.allShots)==0:
             self.allShots = [ x for x in os.listdir(self.seqPath) if unicode(x).isnumeric()]
         else:
             pass
 
-
-
     def getLastAnim(self):
-        
-        #import socket
-        #systemCheck = socket.gethostname()
+
         '''
         if os.name == 'posix':
             animDir = r'03_maya/02_animation'
@@ -135,23 +144,27 @@ class Info():
             for x in os.listdir(shotDir):
                 if 'maya' in x:
                     shotDirMaya = os.path.join( shotDir, x )
-                    print 'shotDirMaya = ' + shotDirMaya
-                else: 
+                    # print 'shotDirMaya:\n' + shotDirMaya
+                else:
                     NameError('maya directory not found in...%s\n    '%shotDir)
-  
+
             for dept in os.listdir(shotDirMaya):
                 if 'animation' in dept:
                     animDir = os.path.join(shotDirMaya, dept)
-                else: NameError('animation directory not found in...%s\n    '%shotDirMaya)
-            print ('animDir: \n\t{}'.format(animDir))
-            maPath = os.path.join(shotDirMaya,animDir)
+                    # print ('animDir:\n{}'.format(animDir))
+                    # print ('\n')
+                else:
+                    NameError('animation directory not found in...%s\n    '%shotDirMaya)
+
+            maPath = os.path.join(shotDirMaya, animDir)
             maFiles = [f for f in os.listdir(maPath) if '.ma' in f if not 'PUBLISH' in f]
             maFiles.sort()
 
-            if maFiles: 
-                lastAnimMAs.append(os.path.join(maPath,maFiles[-1]))
+            if maFiles:
+                lastAnimMAs.append(os.path.join(maPath, maFiles[-1]))
 
-        return lastAnimMAs#, shotDirMaya
+        return lastAnimMAs
+
 
 # SeqInfo('season_01', '101_rain', 'sq100_downpour')
 '''
@@ -165,4 +178,8 @@ if __name__ == '__main__':
     ep, seq, shot = argv[1:]
     print Info(ep, seq, shot).seqPath
     for x in Info(ep, seq, shot).allShots: print x
-'''	
+'''
+
+
+if __name__ == '__main__':
+    main()
---
+++ maya_scripts/python/sbtvKeyMaker.py
@@ -0,0 +1,44 @@
+#!/usr/local/python
+
+# Jib Jab Studios
+# Author: Mark Thielen
+# email:  mdthielen@gmail.com
+# Start date: 3/6/2017
+# Version: 1.0.0
+
+# Usage:
+# To create the sbtvKey.txt which is used by sbtvInfo.py
+# Generates the episode and sequence lists based on the folders in the episode_folder
+
+import os
+
+'''
+Format for key_file:
+
+EPISODE <episode_number> <description>
+SEQ <seq_number> <description>
+
+'''
+
+def main():
+    episode_folder = '/Volumes/public/StoryBots/production/series/ask_the_storybots'
+    episodes = [f for f in os.listdir(episode_folder) if f[3] =='_' if f[0] != '.']
+    key_file = r'/Volumes/public/StoryBots/production/series/ask_the_storybots/03_shared_assets/01_cg/05_maya_tools/' \
+                      r'jj_dev/maya_scripts/python/sbtvKey.txt'
+    stage = '2_production'
+
+    lines = []
+    for episode in sorted(episodes):
+        lines.append('EPISODE ' + episode.split('_')[0] + ' ' + episode.split('_')[1])
+        lines.append('\n')
+        for seq in sorted(os.listdir(os.path.join(episode_folder, episode, stage))):
+            if seq.startswith('sq'):
+                lines.append('SEQ ' + seq.split('_')[0][2:] + ' ' + seq.split('_')[1] + '\n')
+        lines.append('\n\n')
+
+    f = open(key_file, 'w')
+    f.writelines(lines)
+    f.close()
+
+if __name__ == '__main__':
+    main()
