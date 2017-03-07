251efeacfb267be78c1c2abab8e689f4ab1a6a81
mdthielen <mdthielen@gmail.com>
2017-03-06 19:27:52 -0800

    header with docstring footer with credits

--- maya_scripts/python/sbtvKeyMaker.py
+++ maya_scripts/python/sbtvKeyMaker.py
@@ -1,30 +1,25 @@
-#!/usr/local/python
+#!/usr/bin/python
 
-# Jib Jab Studios
-# Author: Mark Thielen
-# email:  mdthielen@gmail.com
-# Start date: 3/6/2017
-# Version: 1.0.0
+"""Generate the sbtvKey.txt from the folders on the server.
 
-# Usage:
-# To create the sbtvKey.txt which is used by sbtvInfo.py
-# Generates the episode and sequence lists based on the folders in the episode_folder
+Usage:
+To create the sbtvKey.txt which is used by sbtvInfo.py
+Generates the episode and sequence lists based on the folders in the episode_folder
 
-import os
-
-'''
 Format for key_file:
 
 EPISODE <episode_number> <description>
 SEQ <seq_number> <description>
+"""
 
-'''
+import os
+
 
 def main():
     episode_folder = '/Volumes/public/StoryBots/production/series/ask_the_storybots'
-    episodes = [f for f in os.listdir(episode_folder) if f[3] =='_' if f[0] != '.']
+    episodes = [f for f in os.listdir(episode_folder) if f[3] == '_' if f[0] != '.']
     key_file = r'/Volumes/public/StoryBots/production/series/ask_the_storybots/03_shared_assets/01_cg/05_maya_tools/' \
-                      r'jj_dev/maya_scripts/python/sbtvKey.txt'
+               r'jj_dev/maya_scripts/python/sbtvKey.txt'
     stage = '2_production'
 
     lines = []
@@ -42,3 +37,13 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+__author__ = "Mark Thielen"
+__copyright__ = "Copyright 2017, Jib Jab Studios"
+__date__ = "3/6/2017"
+__credits__ = ["Mark Thielen"]
+__license__ = "GPL"
+__version__ = "1.0.1"
+__maintainer__ = "Mark Thielen"
+__email__ = "mdthielen@gmail.com"
+__status__ = "Production"
