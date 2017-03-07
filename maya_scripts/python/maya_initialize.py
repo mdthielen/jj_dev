e347ccee6274b7b13e3104652574b362e48b3cc8
mdthielen <mdthielen@gmail.com>
2017-03-07 14:25:28 -0800

    creates userSetup.py if not exists edits userSetup.py creates Maya.env if not exists edits Maya.env with sbtvtools locations and keeps existing paths edits userPlugins.mel for vray plugin currently set to dev on which points to the jj_dev folder

--- maya_scripts/python/maya_initialize.py
+++ maya_scripts/python/maya_initialize.py
@@ -12,13 +12,12 @@ Attributes:
 
 Todo:
     * setup Maya.env
-    * setup userSetup.py
     * setup pluginPrefs.mel
+    * setup userSetup.py
 
 """
 
 import os
-import tempfile
 
 
 def main():
@@ -28,124 +27,206 @@ def main():
     maya_env_file = 'Maya.env'
 
     # VRay plugin
-    vray_plugin_line = 'evalDeferred("autoLoadPlugin(\\\"\\\", \\\"vrayformaya\\\", \\\"vrayformaya\\\")");'
-    replaced_vray = replace_line(maya_prefs_path + maya_plugin_prefs_file, vray_plugin_line, vray_plugin_line)
+    vray_plugin_line = '\nevalDeferred("autoLoadPlugin(\\\"\\\", \\\"vrayformaya\\\", \\\"vrayformaya.bundle\\\")");'
+    replaced_vray = fixLineInFile(maya_prefs_path + maya_plugin_prefs_file, 'vrayformaya', vray_plugin_line)
     if replaced_vray:
         print('Added VRay plugin')
 
     # Maya.env
-    pythonpath = ''
-    maya_scripts_path = ''
-    replaced_maya_env = replace_line(maya_prefs_path + maya_env_file, 'PYTHONPATH', pythonpath)
+    replaced_maya_env = maya_env(maya_prefs_path + maya_env_file, dev = True)
+    if replaced_maya_env:
+        print('Updated Maya.env')
 
+    # userSetup.py
+    replaced_usersetup_py = fixLineInFile(maya_prefs_path + 'userSetup.py', 'import maya.cmds as mc', 'import maya.cmds as mc')
+    if replaced_usersetup_py:
+        print('Updated userSetup.py')
 
-def maya_env(maya_env_file):
+def maya_env(filepath, dev = False):
     """Edits the Maya.env file to have the latest PYTHONPATH, MAYA_SCRIPTS_PATH, and MAYA_SHELF_PATH
 
     Attributes:
         MAYA_SHELF_PATH = /Volumes/public/StoryBots/production/series/ask_the_storybots/03_shared_assets/01_cg/05_maya_tools/maya_shelves/lighting/
         SBTVTOOLS = /Volumes/public/StoryBots/production/series/ask_the_storybots/03_shared_assets/01_cg/05_maya_tools/
-        PYTHONPATH = $SBTVTOOLS/jj_dev/maya_scripts/python:$SBTVTOOLS/jj_dev/maya_scripts/python/RnD
-        MAYA_SCRIPT_PATH = $SBTVTOOLS/jj_dev/maya_scripts/mel
+        PYTHONPATH = $SBTVTOOLS//maya_scripts/python:$SBTVTOOLS/maya_scripts/python/RnD
+        MAYA_SCRIPT_PATH = $SBTVTOOLS/maya_scripts/mel
 
     Returns:
+        True if file written
+        False if error opening file
 
     Todo:
 
     """
     # quick parameter checks
-    assert os.path.exists(filepath)
+    try:
+        assert os.path.exists(filepath)
+    except:
+        with open(filepath, 'w') as f:
+            f.close()
 
-    replaced = False
-    written  = False
+    sbtvtools_var = 'SBTVTOOLS = '
+    maya_script_path_var = 'MAYA_SCRIPT_PATH = '
+    pythonpath_var = 'PYTHONPATH = '
+    maya_shelf_path_var = 'MAYA_SHELF_PATH = '
 
-    maya_shelf_path = '/Volumes/public/StoryBots/production/series/ask_the_storybots/03_shared_assets/01_cg/05_maya_tools/maya_shelves/lighting/'
-    sbtvtools = '/Volumes/public/StoryBots/production/series/ask_the_storybots/03_shared_assets/01_cg/05_maya_tools/'
-    pythonpath = '$SBTVTOOLS/jj_dev/maya_scripts/python:$SBTVTOOLS/jj_dev/maya_scripts/python/RnD'
-    maya_scripts_path = '$SBTVTOOLS/jj_dev/maya_scripts/mel'
+    sbtvtools_path = '/Volumes/public/StoryBots/production/series/ask_the_storybots/03_shared_assets/01_cg/05_maya_tools/'
+    if dev:
+        sbtvtools_path = '/Volumes/public/StoryBots/production/series/ask_the_storybots/03_shared_assets/01_cg/05_maya_tools/jj_dev/'
+    maya_scripts_path = '$SBTVTOOLS/maya_scripts/mel'
+    maya_shelf_path = '$SBTVTOOLS/maya_shelves/lighting/'
+    pythonpath = '$SBTVTOOLS/maya_scripts/python:$SBTVTOOLS/maya_scripts/python/RnD'
+
+    sbtvtools_replaced = False
+    maya_script_path_replaced = False
+    maya_shelf_path_replaced = False
+    pythonpath_replaced = False
 
     try:
-        with open(filepath, 'r+') as f:    # open for read/write -- alias to f
+        with open(filepath, 'r') as f:    # open for read/write -- alias to f
             lines = f.readlines()            # get all lines in file
-            # TODO - check for existing variables in Maya.env
-            # TODO - check if existing path matches correct path
-            # TODO - leave any paths that are user defined and don't overlap studio paths
-            # TODO - put in a dev flag
+            f.close()  # we opened it , we close it
+            lines_replaced = []
 
-    except IOError, ioe:                 # if something bad happened.
+            for line in lines:
+                # sbtvtools
+                if line.startswith(sbtvtools_var):
+                    lines_replaced.append(sbtvtools_var + sbtvtools_path + '\n')
+                    sbtvtools_replaced = True
+                # maya_script_path
+                elif line.startswith(maya_script_path_var):
+                    maya_script_path_old = line.split('=')[1].lstrip()
+                    if ':' in maya_script_path_old:
+                        paths_replaced = []
+                        for path in maya_script_path_old.split(':'):
+                            if '$SBTVTOOLS' in path:
+                                paths_replaced.append(maya_scripts_path)
+                            else:
+                                paths_replaced.append(path)
+                        lines_replaced.append(maya_script_path_var + ':'.join(paths_replaced) + '\n')
+                        maya_script_path_replaced = True
+                    elif '$SBTVTOOLS' in maya_script_path_old:
+                        lines_replaced.append(maya_script_path_var + maya_scripts_path + '\n')
+                        maya_script_path_replaced = True
+                    else:
+                        lines_replaced.append(maya_script_path_var + maya_scripts_path + ':' + maya_script_path_old + '\n')
+                        maya_script_path_replaced = True
+
+                # maya_shelf_path
+                elif line.startswith(maya_shelf_path_var):
+                    maya_shelf_path_old = line.split('=')[1].lstrip()
+                    if ':' in maya_shelf_path_old:
+                        paths_replaced = []
+                        for path in maya_shelf_path_old.split(':'):
+                            if '$SBTVTOOLS' in path:
+                                paths_replaced.append(maya_shelf_path)
+                            else:
+                                paths_replaced.append(path)
+                        lines_replaced.append(maya_shelf_path_var + ':'.join(paths_replaced))
+                        maya_shelf_path_replaced = True
+                    elif '$SBTVTOOLS' in maya_shelf_path_old:
+                        lines_replaced.append(maya_shelf_path_var + maya_shelf_path + '\n')
+                        maya_shelf_path_replaced = True
+                    else:
+                        lines_replaced.append(maya_shelf_path_var + maya_shelf_path + ':' + maya_shelf_path_old.rstrip() + '\n')
+                        maya_shelf_path_replaced = True
+
+                # pythonpath
+                elif line.startswith(pythonpath_var):
+                    pythonpath_old = line.split('=')[1].lstrip()
+                    python_sbtvtools_replaced = False
+                    if ':' in pythonpath_old:
+                        paths_replaced = []
+
+                        for path in pythonpath_old.split(':'):
+                            if '$SBTVTOOLS' in path and not python_sbtvtools_replaced:
+                                paths_replaced.append(pythonpath)
+                                python_sbtvtools_replaced = True
+                            elif '$SBTVTOOLS' not in path:
+                                paths_replaced.append(path)
+                            elif not python_sbtvtools_replaced:
+                                paths_replaced.append(path)
+                        lines_replaced.append(pythonpath_var + ':'.join(paths_replaced) + '\n')
+                        pythonpath_replaced = True
+                    elif '$SBTVTOOLS' in pythonpath_old:
+                        lines_replaced.append(pythonpath_var + pythonpath + '\n')
+                        pythonpath_replaced = True
+                    else:
+                        lines_replaced.append(pythonpath_var + pythonpath + ':' + pythonpath_old + '\n')
+                        pythonpath_replaced = True
+
+                # any other lines
+                else:
+                    lines_replaced.append(line)
+            if not sbtvtools_replaced:
+                lines_replaced.insert(0, sbtvtools_var + sbtvtools_path + '\n')
+            if not maya_script_path_replaced:
+                lines_replaced.insert(1, maya_script_path_var + maya_scripts_path + '\n')
+
+            if not maya_shelf_path_replaced:
+                lines_replaced.insert(2, maya_shelf_path_var + maya_shelf_path + '\n')
+
+            if not pythonpath_replaced:
+                lines_replaced.insert(3, pythonpath_var + pythonpath + '\n')
+
+            with open(filepath, 'w') as f:
+                f.writelines(lines_replaced)
+                f.close()
+                return True
+
+
+    except IOError, ioe:
         printf ("ERROR" , ioe)
-        f.close()
         return False
 
 
-def replace_line(filepath, oldline, newline ):
+def fixLineInFile(filepath, line_keyword, newline):
     """replace a line in a temporary file, then copy it over into the original file if everything goes well
     Attributes:
         filepath file to change
-        oldline old line to replace
+        line_keyword keyword to search
         newline new line to replace
     """
 
     # quick parameter checks
-    assert os.path.exists(filepath)
-    assert (oldline and str(oldline)) # is not empty and is a string
+    try:
+        assert os.path.exists(filepath)
+    except:
+        with open(filepath, 'w') as f:
+            f.close()
+    assert (line_keyword and str(line_keyword)) # is not empty and is a string
     assert (newline and str(newline))
 
-    replaced = False
+    found_plugin = False
     written  = False
 
     try:
-
-        with open(filepath, 'r+') as f:    # open for read/write -- alias to f
-
+        with open(filepath, 'r') as f:    # open for read/write -- alias to f
             lines = f.readlines()            # get all lines in file
-
-            if oldline not in lines:
-                # tmpfile = tempfile.NamedTemporaryFile(delete=True)  # temp file opened for writing
-                #
-                # for line in lines:  # process each line
-                #     tmpfile.write(oldline)  # write old line unchanged
-                # tmpfile.write(newline)  # replace it
-                # replaced = True
-                f.writelines(newline)
-                written = True
-
-                # if replaced:  # overwrite the original file
-                #     f.seek(0)  # beginning of file
-                #     f.truncate()  # empties out original file
-                #     print('writing file')
-                #     for tmplines in tmpfile:
-                #         print tmplines
-                #         f.write(tmplines)  # writes each line to original file
-                #     written = True
-
-            # else:
-            #     tmpfile = tempfile.NamedTemporaryFile(delete=True)  # temp file opened for writing
-            #
-            #     for line in lines:           # process each line
-            #         if line == oldline:        # find the line we want
-            #             tmpfile.write(newline)   # replace it
-            #             replaced = True
-            #         else:
-            #             tmpfile.write(oldline)   # write old line unchanged
-            #
-            #     if replaced:                   # overwrite the original file
-            #         f.seek(0)                    # beginning of file
-            #         f.truncate()                 # empties out original file
-            #
-            #         for tmplines in tmpfile:
-            #             f.write(tmplines)          # writes each line to original file
-            #         written = True
-            #
-            #     tmpfile.close()              # tmpfile auto deleted
             f.close()                          # we opened it , we close it
+            lines_replaced = []
+            for line in lines:
+                if line_keyword not in line:
+                    lines_replaced.append(line.rstrip() + '\n')
+                elif line.startswith('#'):
+                    lines_replaced.append(line.rstrip() + '\n')
+                elif line_keyword in line:
+                    found_plugin = True
+            if not found_plugin:
+                lines_replaced.append(newline)
+                written = True
+            if written:
+                with open(filepath, 'w') as f:
+                    f.writelines(lines_replaced)
+                    f.close()
+                return written
 
     except IOError, ioe:                 # if something bad happened.
         printf ("ERROR" , ioe)
         f.close()
         return False
 
-    return replaced and written        # replacement happened with no errors = True
+    return written        # replacement happened with no errors = True
 
 if __name__ == '__main__':
     main()
